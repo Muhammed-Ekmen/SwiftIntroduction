f0271094302d7d1ccb39cfc56d06ce1808579b22
Muhammed Semih <86803401+Muhammed-Ekmen@users.noreply.github.com>
2023-02-28 23:20:30 +0300

    Update Contents.swift

--- swiftBases.playground/Pages/working.xcplaygroundpage/Contents.swift
+++ swiftBases.playground/Pages/working.xcplaygroundpage/Contents.swift
@@ -1,6 +1,51 @@
 import UIKit
 
+/*
 
+RollingDiceApp{
+setBackground
+motionEnded
+DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()+2)
+}
+
+
+GuessWhatNumberGame{
+UIAlertController
+}
+
+
+SimpleLoginScreen{
+Delegate Usage
+ShakeAnimation
+ErrorHandling
+Extention Usage
+}
+
+
+SimpleTextShareApp{
+Switch
+ProgressBar with TextField
+Share Property
+Indicator
+}
+
+
+OsWarsApp{
+Slider
+perform with afterDelay
+Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block:)
+}
+
+
+SuperHeroApp{
+ViewController
+CollectionView
+Segue
+NavigatorController
+Init Screen
+Cell
+}
+*/
 enum Cars{
     case Ferrari,Maserati
 }
